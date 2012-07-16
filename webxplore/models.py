a862e6289b46313710edd3090ec9962e93abec9f
Malte Lenz <malte.lenz@gmail.com>
2012-07-16 19:38:46 +0200

    Added model definitions for photos and cameras.

--- webxplore/models.py
+++ webxplore/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+class Manufacturer(models.Model):
+    name = models.CharField(max_length = 500)
+    
+    class Meta:
+        verbose_name = 'Manufacturer'
+        verbose_name_plural = 'Manufacturers'
+
+    def __unicode__(self):
+        return self.name
+    
+
+class Camera(models.Model):
+    manufacturer = models.ForeignKey(Manufacturer)
+    name = models.CharField(max_length = 500)
+
+    class Meta:
+        verbose_name = 'Camera'
+        verbose_name_plural = 'Cameras'
+
+    def __unicode__(self):
+        return self.manufacturer + ": " + self.name
+    
+
+class Photo(models.Model):
+    ORIENTATION_CHOICES = (
+        ('LS','Landscape'),
+        ('PT','Portrait'),
+    )
+    
+    path = models.CharField(max_length = 10000)
+    width = models.IntegerField()
+    height = models.IntegerField()
+    exposure = models.IntegerField('Exposure in microseconds')
+    iso = models.IntegerField('ISO speed')
+    orientation = models.CharField(max_length = 2, choices = ORIENTATION_CHOICES)
+    taken_date = models.DateTimeField()
+    fileSize = models.IntegerField('File size in bytes')
+    camera = models.ForeignKey(Camera)
+    focal_length = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'Photo'
+        verbose_name_plural = 'Photos'
+
+    def __unicode__(self):
+        return self.path
+
