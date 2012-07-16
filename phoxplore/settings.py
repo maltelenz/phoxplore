dd825531895f87faf606e3d37c844660648d8d68
Malte Lenz <malte.lenz@gmail.com>
2012-07-16 18:48:26 +0200

    Updated settings and created django app

--- phoxplore/settings.py
+++ phoxplore/settings.py
@@ -9,9 +9,11 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+# XXXX Setup XXXX
+# Update the name below
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
@@ -27,7 +29,9 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+
+# XXXX Setup XXXX
+TIME_ZONE = 'Europe/Stockholm'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
