1f098b9beb117caf48d2e91433136e0c5f3a7ef5
Malte Lenz <malte.lenz@gmail.com>
2012-07-17 20:02:50 +0200

    Import complete folder. Code cleanup.

--- filexplore/im_handler.py
+++ filexplore/im_handler.py
@@ -1,6 +1,7 @@
 import Image
 import hashlib
 import os
+import re
 from time import strptime, mktime
 from datetime import datetime
 
@@ -8,9 +9,12 @@ from PIL.ExifTags import TAGS
 
 from webxplore.models import Photo, Manufacturer, Camera
 
+# Compute a hopefully unique hash based on path,
+# used to save a thumbnail under.
 def file_name_hash(path):
     return hashlib.sha224(path).hexdigest()
 
+# Create and save a thumbnail
 def make_thumbnail(im, outfile, size = (100, 100)):
     try:
         im.thumbnail(size)
@@ -19,8 +23,20 @@ def make_thumbnail(im, outfile, size = (100, 100)):
         return False
     return True
 
+# Create a thumbnail and a database entry for the image
 def import_image(path, thumb_path):
+    # Check if it already exists
+    try:
+        ph = Photo.objects.get(path = path)
+        return ph
+    except Photo.DoesNotExist:
+        # The photo is new, continue with importing
+        pass
+
+    # Compute file name for thumbnail
     outfile = os.path.join(thumb_path, file_name_hash(path) + ".jpg")
+
+    # Collect various data about the image
     im = Image.open(path)
     (width, height) = im.size
     exif_raw = im._getexif()
@@ -45,8 +61,13 @@ def import_image(path, thumb_path):
     manufacturer_name = exif_parsed['Make']
     camera_name = exif_parsed['Model']
 
-    manufacturer, created = Manufacturer.objects.get_or_create(name = manufacturer_name)
-    camera, created = Camera.objects.get_or_create(manufacturer = manufacturer, name = camera_name)
+    manufacturer, created = Manufacturer.objects.get_or_create(
+        name = manufacturer_name
+    )
+    camera, created = Camera.objects.get_or_create(
+        manufacturer = manufacturer,
+        name = camera_name
+    )
 
     focal_length = exif_parsed['FocalLength'][0]
 
@@ -54,8 +75,10 @@ def import_image(path, thumb_path):
 
     iso = exif_parsed['ISOSpeedRatings']
 
+    # Save the thumbnail
     make_thumbnail(im, outfile)
 
+    # Save to database
     ph = Photo(
         path = path,
         width = width,
@@ -68,6 +91,31 @@ def import_image(path, thumb_path):
         camera = camera,
         focal_length = focal_length,
         aperture = aperture
-        ).save()
+    )
     
+    ph.save()
+
     return ph
+
+# Check if a given filename is a file phoxplore can handle
+def is_image_filename(filename):
+    return re.search(r'.[Jj][Pp][Ee]?[Gg]$', filename) != None
+
+# Give a list of images in given path and recursive subdirectories
+def images_in_folder(path):
+    all_files = []
+    for root, dirs, files in os.walk(path):
+        all_files += [os.path.join(root, f)
+                        for f in files
+                            if is_image_filename(f)]
+    return all_files
+
+# Import all images in a folder into phoxplore
+def import_folder(path, thumb_path):
+    images = []
+    for impath in images_in_folder(path):
+        images.append(
+            import_image(impath, thumb_path)
+        )
+
+    return images
--- webxplore/tests.py
+++ webxplore/tests.py
@@ -1,16 +1,21 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
+This file contains tests using the unittest module. These will pass
 when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
 
+from filexplore.im_handler import *
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class ImHandlerTest(TestCase):
+    def test_image_filename_matching(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests that filenames for images are detected correctly
         """
-        self.assertEqual(1 + 1, 2)
+        self.assertEqual(is_image_filename('abc.JPEG'), True)
+        self.assertEqual(is_image_filename('abc.JPG'), True)
+        self.assertEqual(is_image_filename('abc.jpeg'), True)
+        self.assertEqual(is_image_filename('abc.jpg'), True)
+        self.assertEqual(is_image_filename('abc.JPeg'), True)
+        self.assertEqual(is_image_filename('abc.txt'), False)
+        self.assertEqual(is_image_filename('abc.jpg.txt'), False)
